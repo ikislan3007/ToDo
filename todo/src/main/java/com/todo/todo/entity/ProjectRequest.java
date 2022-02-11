f531912cafbad40c7770ef627022943de2b921f5
Irina Kislan <irina.n.kislan@gmail.com>
2022-02-11 10:41:11 +0200

    add validations to name and description

--- todo/src/main/java/com/todo/todo/entity/ProjectRequest.java
+++ todo/src/main/java/com/todo/todo/entity/ProjectRequest.java
@@ -3,10 +3,12 @@ package com.todo.todo.entity;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Data
 public class ProjectRequest {
 
-    @Min(3)
+    @Size(min = 3, max = 200, message
+            = "Name must be between 3 and 200 characters")
     private String name;
 }
--- todo/src/main/java/com/todo/todo/entity/TaskRequest.java
+++ todo/src/main/java/com/todo/todo/entity/TaskRequest.java
@@ -3,14 +3,17 @@ package com.todo.todo.entity;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 @Data
 public class TaskRequest {
 
-    @Min(3)
+    @Size(min = 3, max = 200, message
+            = "name must be between 3 and 200 characters")
     private String name;
 
-    @Min(3)
+    @Size(min = 3, max = 200, message
+            = "Description must be between 10 and 200 characters")
     private String description;
 
     private boolean done;
